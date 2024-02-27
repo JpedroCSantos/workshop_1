3bb2ea2ea5122c07bf1f142c22eec76d00e43d73
Joao Pedro Santos <jpedro.csantos@hotmail.com>
2024-02-26 21:11:39 -0300

    Alterações

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from pipeline.extract import read_data
+from pipeline.transform import concatenate_dataframes
+from pipeline.load import load_excel
 
-data = read_data("./data/input")
-print(data[1])
+
+if __name__ == '__main__':
+    data = read_data("./data/input")
+    dataframe = concatenate_dataframes(data)
+    load_excel(dataframe, "data/output", "output")
---
+++ app/pipeline/load.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+def load_excel(data_frame: pd.DataFrame , output_path: str, filename: str) -> str:
+
+    """
+        Recebe um dataframe e transforma em um arquivo excel
+
+        args:
+            data_frame (pd.dataframe): dataframe a ser convertido em excel
+            output_path (str): caminho onde será salvo o arquivo
+            filename (str): nome do arquivo a ser salvo
+
+        return: "Arquivo salvo com sucesso"
+    """
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    data_frame.to_excel(f"{output_path}/{filename}.xlsx", index= False)
+    return "Arquivo criado com sucesso"
